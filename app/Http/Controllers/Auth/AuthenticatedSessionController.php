00719d18103b5c4ca5da0440e1d249343e346431
YoussefACHCHIRAJ <youssefachchiraj@gmail.com>
2024-08-05 15:49:10 +0100

    feat: create a sample passwordless implementation with laravel

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use App\Notifications\Login;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
@@ -22,11 +25,32 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request)
     {
-        $request->authenticate();
 
-        $request->session()->regenerate();
+        $user = User::where('email', $request->get('email'))->first();
+
+        if (!$user) {
+            return back()->withErrors(['email' => 'No matching account found']);
+        }
+
+        //* generate a temporary url and email the user
+        $link = URL::temporarySignedRoute('login.token', now()->addHour(), ['user' => $user->id]);
+
+        // todo: remove this line later
+        // return $link;
+
+        $user->notify(new Login($link));
+
+        return back()->with(['status' => 'Please check your email for a magic link to login']);
+    }
+
+
+    public function loginViaToken(User $user)
+    {
+        Auth::login($user);
+
+        request()->session()->regenerate();
 
         return redirect()->intended(route('dashboard', absolute: false));
     }
--- routes/auth.php
+++ routes/auth.php
@@ -20,6 +20,9 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
+    Route::get('login/{user}', [AuthenticatedSessionController::class, 'loginViaToken'])
+    ->name('login.token')->middleware('signed');
+
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
